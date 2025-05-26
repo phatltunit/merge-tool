414aea7640c6d9ef72a094ef88c1740efbda988d
PhatLT <phatlt@unit.com.vn>
2025-05-26 14:37:08 +0700

    add support check multiple commit and distinct the changed files

--- gitsupports/gitsupports.go
+++ gitsupports/gitsupports.go
@@ -5,48 +5,84 @@ import (
 	"merge/constants"
 	"merge/objects"
 	"os/exec"
+	"sort"
 	"strings"
+	"sync"
 )
 
-func GetChangedFiles(commitHash string, config objects.Config) {
-	// Run the git command to get the list of changed files
+func checkFileChanged(commitHash string, config objects.Config) []string {
+	hash := strings.Split(commitHash, ",")
+	// Filter out empty strings from the hash slice
+	var filteredHash []string
+	for _, h := range hash {
+		if h != constants.Empty {
+			filteredHash = append(filteredHash, h)
+		}
+	}
+	hash = filteredHash
+
+	var wg sync.WaitGroup
+	fileChan := make(chan []string, len(hash))
+	wg.Add(len(hash)) // add the number of goroutines to wait for
+	for _, commit := range hash {
+		singleCheck(commit, config, fileChan, &wg)
+	}
+	wg.Wait()       // wait for all goroutines to finish
+	close(fileChan) // this is important to close the channel after all goroutines are done
+	// Collect all files from the channel
+	var allFiles []string
+	for files := range fileChan {
+		allFiles = append(allFiles, files...)
+	}
+	return allFiles
+}
+
+func singleCheck(commitHash string, config objects.Config, fileChan chan []string, wg *sync.WaitGroup) {
+	defer wg.Done() // signal that this goroutine is done
+	// Run the git command to check if the file has changed
 	cmd := exec.Command("git", "show", "--pretty=", "--name-only", commitHash)
 	cmd.Dir = config.GitRepo
 	output, err := cmd.Output()
 	if err != nil {
 		fmt.Println(err)
+		return
 	}
 
-	// Split the output into lines and return as a slice of strings
+	// Split the output into lines and check if the file is in the list
 	files := strings.Split(string(output), constants.BreakLine)
-	fmt.Println(constants.Empty)
 	// concat files with git repo to get full output path
 	for i, file := range files {
 		if file != constants.Empty {
 			files[i] = config.GitRepo + constants.PathSeparator + file
-			// fmt.Println(files[i])
 		}
 	}
 
-	orderedFiles := orderedFiles(files)
+	fileChan <- files
+}
 
+func GetChangedFiles(commitHash string, config objects.Config) {
+	files := checkFileChanged(commitHash, config)
+	orderedFiles := orderedFiles(files)
 	for _, file := range orderedFiles {
 		fmt.Println(file)
 	}
-
 	fmt.Println(constants.Empty)
 }
 
 func orderedFiles(files []string) []string {
 	// Sort the files in alphabetical order
-	for i := 0; i < len(files)-1; i++ {
-		for j := i + 1; j < len(files); j++ {
-			if files[i] > files[j] {
-				files[i], files[j] = files[j], files[i]
-			}
+	distinctFiles := make(map[string]bool, len(files))
+	orderedFiles := make([]string, 0, len(files))
+
+	for _, file := range files {
+		if _, ok := distinctFiles[file]; !ok {
+			distinctFiles[file] = true
+			orderedFiles = append(orderedFiles, file)
 		}
 	}
-	return files
+
+	sort.Strings(orderedFiles)
+	return orderedFiles
 }
 
 func ExecGitCommand(command string, config objects.Config) {

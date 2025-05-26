a09fdde73ccd4981fd0ccfd305def835a3d9c14a
PhatLT <phatlt@unit.com.vn>
2025-05-26 09:36:20 +0700

    remove unused constant and update output message in main function

--- main.go
+++ main.go
@@ -7,13 +7,10 @@ import (
 	"time"
 )
 
-
-
-
 func main() {
 	startTime := time.Now()
 	fmt.Println(constants.Message)
-	fmt.Println(constants.BreakLine)
+	fmt.Println(constants.Empty)
 	logics.MainLogic()
 	endTime := time.Now()
 	fmt.Printf("We took: %v ms for the job\n", endTime.Sub(startTime).Milliseconds())
